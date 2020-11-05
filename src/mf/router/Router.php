20ef473d2eca4c6b5d6f335d9e07e407c3767578
Benjamin <ben88200@gmail.com>
2020-11-05 09:49:55 +0100

    Ajout niveau d'accès dans addRoute()

--- src/mf/router/Router.php
+++ src/mf/router/Router.php
@@ -2,38 +2,43 @@
 
 namespace mf\router;
 
-class Router extends \mf\router\AbstractRouter {
+class Router extends \mf\router\AbstractRouter
+{
 
-    public function addRoute($name, $url, $ctrl, $mth) {
-        self::$routes[$url] = [$ctrl, $mth];
+    public function addRoute($name, $url, $ctrl, $mth, $access_level)
+    {
+        self::$routes[$url] = [$ctrl, $mth, $access_level];
         self::$aliases[$name] = $url;
     }
 
-    public function setDefaultRoute($url) {
+    public function setDefaultRoute($url)
+    {
         self::$aliases['default'] = $url;
     }
 
-    public function run() {
+    public function run()
+    {
         $path_info = $this->http_req->path_info;
-            if (isset(self::$routes[$path_info])) {
-                $ctrl_name = self::$routes[$path_info][0]; 
-                $mth_name = self::$routes[$path_info][1];
-                $c = new $ctrl_name();
-                $c->$mth_name();
-            } else {
-                $default_url = self::$aliases['default'];
-                $ctrl_name = self::$routes[$default_url][0]; 
-                $mth_name = self::$routes[$default_url][1];
-                $c = new $ctrl_name();
-                $c->$mth_name();
-            }
+        if (isset(self::$routes[$path_info])) {
+            $ctrl_name = self::$routes[$path_info][0];
+            $mth_name = self::$routes[$path_info][1];
+            $c = new $ctrl_name();
+            $c->$mth_name();
+        } else {
+            $default_url = self::$aliases['default'];
+            $ctrl_name = self::$routes[$default_url][0];
+            $mth_name = self::$routes[$default_url][1];
+            $c = new $ctrl_name();
+            $c->$mth_name();
+        }
     }
-    
-    public function urlFor($route_name, $param_list=[]) {
+
+    public function urlFor($route_name, $param_list = [])
+    {
         $href = $this->http_req->script_name;
         $url = self::$aliases[$route_name];
         $href = $href . $url;
-        if(count($param_list) > 0) {
+        if (count($param_list) > 0) {
             $href .= "?";
             foreach ($param_list as $value) {
                 $href .= $value[0];
@@ -45,8 +50,7 @@ class Router extends \mf\router\AbstractRouter {
         return $href;
     }
 
-    public function executeRoute() {
-
+    public function executeRoute()
+    {
     }
-
 }
