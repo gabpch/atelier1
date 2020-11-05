ea352114f823b9bde36a1d1ddf941df9101a9a97
Benjamin <ben88200@gmail.com>
2020-11-05 14:23:05 +0100

    MAJ Framework Router

--- src/mf/router/Router.php
+++ src/mf/router/Router.php
@@ -2,73 +2,58 @@
 
 namespace mf\router;
 
-class Router extends \mf\router\AbstractRouter
+class Router extends AbstractRouter
 {
-
-    public function addRoute($name, $url, $ctrl, $mth, $access_level)
+    public function __construct()
     {
-        self::$routes[$url] = [$ctrl, $mth, $access_level];
-        self::$aliases[$name] = $url;
-    }
-
-    public function setDefaultRoute($url)
-    {
-        self::$aliases['default'] = $url;
+        parent::__construct();
     }
 
     public function run()
     {
-        $path_info = $this->http_req->path_info;
-
-        if (isset(self::$routes[$path_info])) {
-            $auth = new \mf\auth\Authentification;
-
-            if ($auth->checkAccessRight(self::$routes[$path_info][2])) {
-                $ctrl_name = self::$routes[$path_info][0];
-                $mth_name = self::$routes[$path_info][1];
-            } else {
-                $default_url = self::$aliases['default'];
-                $ctrl_name = self::$routes[$default_url][0];
-                $mth_name = self::$routes[$default_url][1];
+        $path = $this->http_req->path_info;
+        if (array_key_exists($path, self::$routes)) {
+            $auth = new \mf\auth\Authentification();
+            if ($auth->checkAccessRight(self::$routes[$path][2])) {
+                $cname = self::$routes[$path][0];
+                $cmth = self::$routes[$path][1];
             }
-
-            $c = new $ctrl_name();
-            $c->$mth_name();
+        } else {
+            $cname = self::$routes[self::$aliases['default']][0];
+            $cmth = self::$routes[self::$aliases['default']][1];
         }
-
-        // $path = $this->http_req->path_info;
-        // if (array_key_exists($path, self::$routes)) {
-        //     $auth = new \mf\auth\Authentification();
-        //     if ($auth->checkAccessRight(self::$routes[$path][2])) {
-        //         $cname = self::$routes[$path][0];
-        //         $cmth = self::$routes[$path][1];
-        //     }
-        // } else {
-        //     $cname = self::$routes[self::$aliases['default']][0];
-        //     $cmth = self::$routes[self::$aliases['default']][1];
-        // }
-        // $c = new $cname();
-        // $c->$cmth();
+        $c = new $cname();
+        $c->$cmth();
     }
-
     public function urlFor($route_name, $param_list = [])
     {
-        $href = $this->http_req->script_name;
-        $url = self::$aliases[$route_name];
-        $href = $href . $url;
-        if (count($param_list) > 0) {
-            $href .= "?";
-            foreach ($param_list as $value) {
-                $href .= $value[0];
-                $href .= "=";
-                $href .= $value[1];
-            }
-            // a faire cas multi parametres
-        }
-        return $href;
-    }
+        if (isset(self::$aliases[$route_name])) {
+            $url_alias = self::$aliases[$route_name];
+            $url = $this->http_req->script_name . $url_alias;
 
-    public function executeRoute()
+            if ($param_list != null) {
+                $url .= "?";
+                foreach ($param_list as $param)
+                    $url = $url . $param[0] . "=" . $param[1];
+            }
+            return $url;
+        }
+    }
+    public function setDefaultRoute($url)
     {
+        self::$aliases['default'] = $url;
+    }
+    public function addRoute($name, $url, $ctrl, $mth, $level)
+    {
+        self::$routes[$url] = array($ctrl, $mth, $level);
+        self::$aliases[$name] = $url;
+    }
+    static public function executeRoute($alias)
+    {
+        $rname = self::$aliases[$alias];
+        $cname = self::$routes[$rname][0];
+        $cmth = self::$routes[$rname][1];
+        $c = new $cname();
+        $c->$cmth();
     }
 }
