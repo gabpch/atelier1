28707c92d1b332122cf1bbcf824bdc6c52d45998
Benjamin <ben88200@gmail.com>
2020-11-05 11:42:48 +0100

    createUser() -> OK

--- src/galleryapp/auth/GalleryAuthentification.php
+++ src/galleryapp/auth/GalleryAuthentification.php
@@ -14,9 +14,21 @@ class GalleryAuthentification extends \mf\auth\Authentification
     const ACCESS_LEVEL_VISITOR  = 0;
     const ACCESS_LEVEL_USER = 1;
 
-    public function createUser($username, $pass, $fullname, $level = self::ACCESS_LEVEL_USER)
+    public function createUser($name,  $first_name, $mail, $pass, $username)
     {
+        $user = User::select()->where('user_name', '=', $username)->first();
+
+        if ((!isset($user->user_name)) || $user->user_name !== $username) {
+            $addUser = new User();
+            $addUser->name = $name;
+            $addUser->first_name = $first_name;
+            $addUser->mail = $mail;
+            $addUser->password = password_hash($pass, PASSWORD_DEFAULT);
+            $addUser->user_name = $username;
+            $addUser->save();
+        }
     }
+
 
     public function loginUser($username, $password)
     {
