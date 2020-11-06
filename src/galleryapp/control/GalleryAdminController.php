b8354cd0d43cef0dbc71cb95aab05bbe232444c1
Benjamin <ben88200@gmail.com>
2020-11-06 14:17:36 +0100

    Ajout méthodes: login et checkLogin

--- src/galleryapp/control/GalleryAdminController.php
+++ src/galleryapp/control/GalleryAdminController.php
@@ -2,10 +2,25 @@
 
 namespace galleryapp\control;
 
+use galleryapp\auth\GalleryAuthentification;
+use galleryapp\view\GalleryView;
+
 class GalleryAdminController extends \mf\control\AbstractController
 {
     public function __construct()
     {
         parent::__construct();
+    }
+
+    public function login()
+    {
+        $vue = new GalleryView(null);
+        $vue->render('auth');
+    }
+
+    public function checkLogin()
+    {
+        $auth = new GalleryAuthentification();
+        $auth->loginUser($this->request->post['user'], $this->request->post['password'], null);
     }
 }
