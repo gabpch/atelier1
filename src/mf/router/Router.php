a1bf7510794ed71ddafb172b7c8380cd34da2fb2
Benjamin <ben88200@gmail.com>
2020-11-06 11:29:04 +0100

    Maj Framework Router

--- src/mf/router/Router.php
+++ src/mf/router/Router.php
@@ -1,45 +1,42 @@
 <?php
 
-    namespace mf\router;
+namespace mf\router;
+
 class Router extends AbstractRouter
 {
-    public function __construct()
+    public function construct()
     {
-        parent::__construct();
+        parent::construct();
     }
 
     public function run()
     {
         $path = $this->http_req->path_info;
-        if(array_key_exists($path,self::$routes))
-        {
+        if (array_key_exists($path, self::$routes)) {
             $auth = new \mf\auth\Authentification();
-            if($auth->checkAccessRight(self::$routes[$path][2]))
-            {
+            if ($auth->checkAccessRight(self::$routes[$path][2])) {
                 $cname = self::$routes[$path][0];
                 $cmth = self::$routes[$path][1];
             }
-        }
-        else
-        {
+            $cname = self::$routes[self::$aliases['default']][0];
+            $cmth = self::$routes[self::$aliases['default']][1];
+        } else {
             $cname = self::$routes[self::$aliases['default']][0];
             $cmth = self::$routes[self::$aliases['default']][1];
         }
         $c = new $cname();
         $c->$cmth();
     }
-    public function urlFor($route_name, $param_list=[])
+    public function urlFor($route_name, $param_list = [])
     {
-        if(isset(self::$aliases[$route_name]))
-        {
+        if (isset(self::$aliases[$route_name])) {
             $url_alias = self::$aliases[$route_name];
-            $url = $this->http_req->script_name. $url_alias;
-    
-            if($param_list != null)
-            {
-                $url .="?";
-                foreach($param_list as $param)
-                    $url = $url.$param[0]."=".$param[1];
+            $url = $this->http_req->script_name . $url_alias;
+
+            if ($param_list != null) {
+                $url .= "?";
+                foreach ($param_list as $param)
+                    $url = $url . $param[0] . "=" . $param[1];
             }
             return $url;
         }
@@ -48,9 +45,9 @@ class Router extends AbstractRouter
     {
         self::$aliases['default'] = $url;
     }
-    public function addRoute($name, $url, $ctrl, $mth,$level)
+    public function addRoute($name, $url, $ctrl, $mth, $level)
     {
-        self::$routes[$url] = array($ctrl, $mth,$level);
+        self::$routes[$url] = array($ctrl, $mth, $level);
         self::$aliases[$name] = $url;
     }
     static public function executeRoute($alias)
